69ebfcf12c86064ba08ec8948a0a61fa4d239039
maxvianna <emaildumax@gmail.com>
2014-10-17 11:25:17 -0300

    Segunda parte do tutorial authcomponent (validação)

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -3,5 +3,23 @@
  
       public $useTable = 'posts';
 
+      public $validate = array(
+			'title'=>array(
+				'title_must_not_be_blank'=>array(
+					'rule'=>'notEmpty',
+					'message'=>'This post is missing a Title'
+				),
+				'title_must_be_unique'=>array(
+					'rule'=>'isUnique',
+					'message'=>'A post with this title already exists!'
+				)
+			),
+			'body'=>array(
+				'body_must_not_be_blank'=>array(
+					'rule'=>'notEmpty',
+					'message'=>'This post is missing its body'
+				)
+			)			
+		);
    }
 ?>
--- app/Model/User.php
+++ app/Model/User.php
@@ -3,5 +3,55 @@
  
       public $useTable = 'users';
 
+      public $validate = array(
+			'name' => array(
+				'Please enter your name' => array(
+					'rule' => 'notEmpty',
+					'message' => 'Please enter your name'
+				)
+			),
+			'username' => array(
+				'The username must be between 5 and 15 characters' => array(
+					'rule' => array('between', 5, 15),
+					'message' => 'The username must be between 5 and 15 characters'
+				), 
+				'That username has already been token' => array(
+					'rule' => 'isUnique',
+					'message' => 'That username has already been token'
+				)
+			),
+			'email' => array(
+				'Valid email' => array(
+					'rule' => array('email'),
+					'message' => 'Please enter a valid email address'
+				)
+			),
+			'password' => array(
+				'Not empty' => array(
+					'rule' => 'notEmpty',
+					'message' => 'Please enter your password'
+				),
+				'Match passwords' => array(
+					'rule' => 'matchPasswords',
+					'message' => 'Your passwords do not match'
+				)
+			),
+			'password_confirmation' => array(
+				'Not empty' => array(
+					'rule' => 'notEmpty',
+					'message' => 'Please confirm your password'
+				)
+			)
+		);
+		
+		public function matchPasswords($data)
+		{
+			if($data['password'] == $this->data['User']['password_confirmation'])
+			{
+				return TRUE;
+			}
+			$this->invalidate('password_confirmation', 'Your passwords do not match');
+			return FALSE;
+		}
    }
 ?>
